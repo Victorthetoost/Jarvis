3e49a2f889338b941e091ab2a4e199e67bfb9ea3
Victor <vvoznyuk@purdue.edu>
2025-08-04 17:45:40 -0700

    startign to use kivy we will see how that goes

--- front_end.py
+++ front_end.py
@@ -1,2 +1,16 @@
-import tkinter
-from tkinter import messagebox
+import kivy
+import kivy.uix.screenmanager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+kivy.require('2.0.0')
+class Application(App):
+    def build(self):
+        self.title = "My Kivy App"
+        return Label(text="Hello, Kivy!")
+        return Builder.load_file('front_end.kv')
+
+
